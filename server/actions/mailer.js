42ed9fe7ae0ab43d25926b13356dbffbbfc0aaa1
Jaromir <jaromircharles@hotmail.com>
2020-07-05 17:43:50 +0200

    changed mailer back to transporter

--- server/actions/mailer.js
+++ server/actions/mailer.js
@@ -9,7 +9,7 @@ const mailer = {
     const emailBody = `Hi there,<br/><br/><b>${tenant}</b> would like you to join their Personnel Management System to always be up to date with their current jobs. Click the following link to register to <b>${tenant}'s</b> PMS.<br/><br/><a href=\"http://localhost:3000/register/${tenant}\">Register now</a>`;
     const options = { ...mailOptions, to: recipient, html: emailBody };
     //console.log(options);
-    mailtrapTransporter.sendMail(options, function (err, info) {
+    transporter.sendMail(options, function (err, info) {
       if (err) {
         console.error("unable to send confirmation email" + err);
       }
